cfa5818d1293b6e66b359e6fca50211df5c56900
Ateeb Sheikh <Ateeb.Sheikh@CSPKAM00000843.mLogica.com>
2024-05-08 18:02:27 +0500

    updated

--- src/pages/Card.js
+++ src/pages/Card.js
@@ -1,103 +1,105 @@
-import Card from "react-bootstrap/Card";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/esm/Container";
-// import CardData from "../Json_data/Card_data.json";
-// import { useEffect } from "react";
+import { Container, Button, Col, Card, Row, Modal } from "react-bootstrap";
+import CardData from "../Json_data/Card_data.json";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function GridExample() {
-  // useEffect(() => {
-  //   console.log(CardData)
-  // },[]) 
+  const [show, setShow] = useState(false);
+  const [getData, SetData] = useState([]);
+  const [getModalData, SetDataModalData] = useState([])
+
+  const handleClose = () => setShow(false);
+  const handleShow = (id) =>{
+    setShow(true);
+    console.log(id)
+
+  }
+  useEffect(() => {
+    SetData(CardData)
+  },[])
   // return (
 
-  
-  const cardData = [
-    {
-      id: 1,
-      imageSrc: 'blue-audi.jpg',
-      title: 'Audi-e-Tron',
-      description: 'The Audi e-tron represents the pinnacle of electric vehicle innovation, offering a harmonious blend of cutting-edge technology.',
-      lastUpdated: 'Last updated 30 mins ago'
-    },
-    {
-      id: 2,
-      imageSrc: 'red-audi.jpg',
-      title: 'Audi SportBack',
-      description: 'The Audi e-tron Sportback merges sleek, aerodynamic design with the electrifying performance of an all-electric powertrain.',
-      lastUpdated: 'Last updated 29 mins ago'
-    },
-    {
-      id: 3,
-      imageSrc: 'green-audi.jpg',
-      title: 'Audi S3 Sedan',
-      description: 'The Audi S3 Sedan exudes sporty sophistication, combining exhilarating performance with luxurious comfort and cutting-edge technology',
-      lastUpdated: 'Last updated 40 min ago'
-    },
-    {
-      id: 4,
-      imageSrc: 'black-audi.jpg',
-      title: 'Audi A3 Saloon',
-      description: 'The Audi A3 Saloon epitomizes compact luxury, delivering a perfect balance of refined elegance, advanced technology.',
-      lastUpdated: 'Last updated 10 min ago'
-    }
-    
-  ];
+  // const cardData = [
+  //   {
+  //     id: 1,
+  //     imageSrc: "blue-audi.jpg",
+  //     title: "Audi-e-Tron",
+  //     description:
+  //       "The Audi e-tron represents the pinnacle of electric vehicle innovation, offering a harmonious blend of cutting-edge technology.",
+  //     lastUpdated: "Last updated 30 mins ago",
+  //   },
+  //   {
+  //     id: 2,
+  //     imageSrc: "red-audi.jpg",
+  //     title: "Audi SportBack",
+  //     description:
+  //       "The Audi e-tron Sportback merges sleek, aerodynamic design with the electrifying performance of an all-electric powertrain.",
+  //     lastUpdated: "Last updated 29 mins ago",
+  //   },
+  //   {
+  //     id: 3,
+  //     imageSrc: "green-audi.jpg",
+  //     title: "Audi S3 Sedan",
+  //     description:
+  //       "The Audi S3 Sedan exudes sporty sophistication, combining exhilarating performance with luxurious comfort and cutting-edge technology",
+  //     lastUpdated: "Last updated 40 min ago",
+  //   },
+  //   {
+  //     id: 4,
+  //     imageSrc: "black-audi.jpg",
+  //     title: "Audi A3 Saloon",
+  //     description:
+  //       "The Audi A3 Saloon epitomizes compact luxury, delivering a perfect balance of refined elegance, advanced technology.",
+  //     lastUpdated: "Last updated 10 min ago",
+  //   },
+  // ];
 
   return (
     <Container className="ca">
-      <Row xs={1} md={2} className="g-4">
-        {cardData.map(card => (
-          <Col key={card.id} >
-            <Card className="card1">
-              <Card.Img variant="top" src={card.imageSrc} className="border border-dark" />
-              <div className="carousel-overlay2"></div>
+      <Row xs={1} md={2} lg={2} sm={1} className="g-4">
+        {getData.map((card) => (
+          <Col key={card.id}>
+            <Card>
+              <Card.Img
+                variant="top"
+                src={card.imageSrc}
+                className="border border-dark"
+              />
+              {/* <div className="carousel-overlay2"></div> */}
               <Card.Body>
                 <Card.Title>{card.title}</Card.Title>
                 <Card.Text>{card.description}</Card.Text>
               </Card.Body>
-              <Card.Footer>
+              <Button  className="d-flex justify-content-center"  id="ok" onClick={()=>handleShow(card.id)}>
+                  Specification
+                </Button>
+              <Card.Footer className="d-flex justify-content-center">
                 <small>{card.lastUpdated}</small>
-              </Card.Footer>
+                
+              </Card.Footer>             
             </Card>
           </Col>
         ))}
       </Row>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title></Modal.Title>
+        </Modal.Header>
+        <Modal.Body></Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary">Understood</Button>
+        </Modal.Footer>
+      </Modal>
     </Container>
- 
-
-
-
-
-
-
-
-
-    // <Container className="ca">
-    //   <Row>
-    //       <Col md={6} xs={12} lg={6} xl={6} sm={12}>
-    //         <Card className="card1">
-    //           <Card.Img
-    //             variant="top"
-    //             src="red-audi.jpg"
-    //             class="border border-dark"
-    //           />
-    //           <div className="carousel-overlay2"></div>
-    //           <Card.Body>
-    //             <Card.Title>Audi SportBack</Card.Title>
-    //             <Card.Text>
-    //               The Audi e-tron Sportback merges sleek, aerodynamic design
-    //               with the electrifying performance of an all-electric
-    //               powertrain.
-    //             </Card.Text>
-    //           </Card.Body>
-    //           <Card.Footer>
-    //             <small>Last updated 29 mins ago</small>
-    //           </Card.Footer>
-    //         </Card>
-    //       </Col>
-    //   </Row>
-    // </Container>
   );
 }
 
---
+++ src/pages/Price.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+
+
+function Price() {
+ 
+
+  return (
+    <>
+      
+
+      
+     
+    </>
+  );
+}
+
+export default Price;
+
+
+
+
+
+
+
